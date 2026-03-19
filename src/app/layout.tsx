ea023683d245324f98520ebc3c7aeb093633029f
Clarence Vince Razo <clarencevinceducorazo@gmail.com>
2026-03-19 08:10:54 +0000

    Console Error
    A tree hydrated but some attributes of the server rendere

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=JetBrains+Mono:wght@300;400;500;600&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-[#050810] text-[#eef2ff]">
+      <body className="font-body antialiased bg-[#050810] text-[#eef2ff]" suppressHydrationWarning>
         <Providers>
           {children}
         </Providers>
